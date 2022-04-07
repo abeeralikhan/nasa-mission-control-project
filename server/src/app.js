d3075f59cf0aef8b81a61b9afb0c8a90d2274b00
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-04-07 18:23:43 +0500

    make /planets end-point on server and fetch it from the client

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
+const planetsRouter = require('./routes/planets/planets.route');
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
---
+++ server/src/models/planets.model.js
@@ -0,0 +1,3 @@
+planets = [];
+
+module.exports = planets;
