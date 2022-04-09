bbee43b655e2f1f3c0aaf8e7591b567c267e3727
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-04-09 16:57:07 +0500

    /get planets

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,51 @@
-planets = [];
+const { parse } = require('csv-parse');
+const fs = require('fs');
+const path = require('path');
 
-module.exports = planets;
+const habitablePlanets = [];
+
+// to filter records
+function isHabitable(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+    && planet['koi_prad'] < 1.6;
+}
+// Reading data from csv file
+// readable.pipe(writeable)
+/*
+const promise = new Promise((resolve, reject) => {
+    resolve(42);
+});
+
+promise.then((result) => {
+
+});
+const result = await promise;
+*/
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#', // signifies that the comments are declared using the pound symbol in the csv file
+            columns: true, // signifies that the data should be javascript object instead of an array 
+        }))
+        .on('data', (data) => {
+            if (isHabitable(data)) habitablePlanets.push(data);
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve();
+        })
+    });
+}
+
+
+module.exports = {
+    planets: habitablePlanets,
+    loadPlanetsData
+}
--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,8 @@
 const app = require('./app');
+
 const http = require('http');
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 // process.env --> returns user enviroment
 const PORT = process.env.PORT || 8000;
@@ -7,10 +10,18 @@ const PORT = process.env.PORT || 8000;
 // we're creating a server using express as a middleware
 const server = http.createServer(app);
 
-// express and http has same listening function
-server.listen(PORT, () => {
-    console.log(`Server is listening at port ${PORT}`);
-});
+// We're using a aync function here because without it, we cannot use the await keyword
+// Below is a very common pattern for a node app
+async function startServer() {
+    await loadPlanetsData();
+
+    // express and http has same listening function
+    server.listen(PORT, () => {
+        console.log(`Server is listening at port ${PORT}`);
+    });
+}
+
+startServer();
 
 
 
