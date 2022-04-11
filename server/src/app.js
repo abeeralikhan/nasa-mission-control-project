926ecaf21648fa98c8afdc6899079d8216482a50
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-04-11 22:03:49 +0500

    add morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -8,7 +9,9 @@ const planetsRouter = require('./routes/planets/planets.route');
 
 app.use(cors({
     origin: 'http://localhost:3000'
-}));
+})); // middleware for cross-origin-policy
+app.use(morgan('combined')); // middleware for managing logs
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
